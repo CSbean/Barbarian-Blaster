43519aec1d11af8aebf62730de89334c2674f041
CSbean <2261060@flourbluffschools.org>
2026-01-22 12:40:35 -0600

    rotating turret

--- Scenes/turret_manager.gd
+++ Scenes/turret_manager.gd
@@ -1,10 +1,11 @@
 extends Node3D
 class_name TurretManager
 
-@export var turret : PackedScene
-
+@export var turret :PackedScene
+@export var enemy_path :Path3D
 
 func build_turret(pos : Vector3)-> void:
 	var new_turret = turret.instantiate()
 	add_child(new_turret)
 	new_turret.global_position = pos
+	new_turret.enemy_path = enemy_path
--- turret/turret.gd
+++ turret/turret.gd
@@ -2,11 +2,13 @@ extends Node3D
 
 @onready var timer: Timer = $Timer
 
-
+var enemy_path :Path3D
 var projectile = preload("res://turret/projectile.tscn")
 
 
-
+func _physics_process(delta: float) -> void:
+	var enemy = enemy_path.get_children().back()
+	look_at(enemy.global_position, Vector3.UP, true)
 
 func _on_timer_timeout() -> void:
 	var new_proj = projectile.instantiate()
