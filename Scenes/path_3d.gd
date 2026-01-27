48e08e7c3ea87691ca3c178836d2514f9bf4913e
CSbean <2261060@flourbluffschools.org>
2026-01-27 12:39:54 -0600

    added spawn curves

--- Scenes/path_3d.gd
+++ Scenes/path_3d.gd
@@ -2,12 +2,12 @@ extends Path3D
 
 @export var enemy_scene : PackedScene
 @onready var timer: Timer = $Timer
-
+@export var dificulty_manager : Node
 
 func spawn_enemy()->void:
 	var new_enemy = enemy_scene.instantiate()
 	add_child(new_enemy)
-
+	timer.wait_time = dificulty_manager.get_spawn_time()
 
 
 
---
+++ dificulty Mangaer/dificulty_manager.gd
@@ -0,0 +1,23 @@
+extends Node
+
+@onready var timer: Timer = $Timer
+@export var spawn_time_curve: Curve
+@export var game_length := 30.0
+@export var enemy_health_curve: Curve
+
+func _ready() -> void:
+	timer.start(game_length)
+
+
+func _process(_delta: float) -> void:
+	pass
+
+func game_progress_ratio()-> float:
+	return 1.0 - (timer.time_left/game_length)
+
+func get_spawn_time()->float:
+	return spawn_time_curve.sample(game_progress_ratio())
+
+#need to retuen a float
+func get_enemy_health()->void:
+	pass
