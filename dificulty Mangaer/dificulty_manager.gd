7f2509ba74f5a4eec9b0e6355ea29522b99f0678
CSbean <2261060@flourbluffschools.org>
2026-01-28 12:26:27 -0600

    dificulty curves added

--- dificulty Mangaer/dificulty_manager.gd
+++ dificulty Mangaer/dificulty_manager.gd
@@ -19,5 +19,5 @@ func get_spawn_time()->float:
 	return spawn_time_curve.sample(game_progress_ratio())
 
 #need to retuen a float
-func get_enemy_health()->void:
-	pass
+func get_enemy_health()->float:
+	return enemy_health_curve.sample(game_progress_ratio())
